d33db4aa9bbe97d07ef12e9b3f675ca44b95ef5e
Madan Kumar Rajan <madan@seattlehomepro.com>
2015-12-01 15:23:24 +0530

    Adding more info to expt run configs

--- src/clj/soprano/experiment.clj
+++ src/clj/soprano/experiment.clj
@@ -15,6 +15,8 @@
   {:experiment-code "my-experiment"
    :start-time "2015-12-03"
    :end-time "2015-12-13"
+   :created-by "Lokesh"
+   :created-time "2015-12-02"
    :option-configuration {:blue {:probability 0.2}
                           :red {:probability 0.5}
                           :green {:probability 0.3
